fb23ed63d9887d59c951d2113908f3f0ed7e2bf8
Ramil Farkhshatov <ramil@gmx.co.uk>
2013-11-19 22:18:16 +0400

    Updated simple profiler.

--- profile.c
+++ profile.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <time.h>
 
+#define PROFILE_C
 #include "profile.h"
 
 static unsigned int n[PROF_END] = {0};
@@ -10,19 +11,27 @@ static unsigned int start[PROF_END] = {0};
 void
 profile_start(int track)
 {
-  if (track < 0 || track >= PROF_END || start[track])
-    return;
-  start[track] = clock();
+  if (track >= 0 && track < PROF_END && !start[track])
+    start[track] = clock();
+}
+
+void
+profile_end2(int track)
+{
+  if (track >= 0 && track < PROF_END && start[track]) {
+    times[track] += clock() - start[track];
+    start[track] = 0;
+  }
 }
 
 void
 profile_end(int track)
 {
-  if (track < 0 || track >= PROF_END)
-    return;
-  n[track]++;
-  times[track] += clock() - start[track];
-  start[track] = 0;
+  if (track >= 0 && track < PROF_END && start[track]) {
+    ++n[track];
+    times[track] += clock() - start[track];
+    start[track] = 0;
+  }
 }
 
 void
@@ -31,8 +40,10 @@ profile_show(void)
   int i;
   double t;
 
+  printf("%2s\t%10s\t%5s\t%8s\t%8s\n", "Id", "Tag", "N", "T/N(s)", "T(s)");
   for (i = 0; i < PROF_END; ++i) {
     t = (double)times[i] / CLOCKS_PER_SEC;
-    printf("%2d\t%4d\t%8f\t%8f\n", i, n[i], (n[i]) ? t / n[i] : -1, t);
+    printf("%2d\t%10s\t%5d\t%8f\t%8f\n", i, profile_tags[i], n[i],
+           (n[i]) ? t / n[i] : -1, t);
   }
 }
--- profile.h
+++ profile.h
@@ -2,13 +2,42 @@ enum {
   PROF_LOOP = 0,
   PROF_EVENTS,
   PROF_IO,
+  PROF_IO_READ,
+  PROF_IO_UNPACK,
+  PROF_IO_PACK,
+  PROF_IO_PROCESS,
+  PROF_IO_DEFERRED,
+  PROF_IO_UPD_FDSET,
+  PROF_IO_CLIENT,
+  PROF_IO_MEMMOVE,
+  PROF_IO_SEND,
   PROF_DRAW,
   PROF_DRAW_BLIT,
-  PROF_DRAW_VIEW,
-  PROF_UPDATE_VIEW,
+  PROF_UPDATE,
+  PROF_UPDATE_POS,
   PROF_END
+};
+
+static const char *profile_tags[] = {
+  [PROF_LOOP] = "loop",
+  [PROF_EVENTS] = "events",
+  [PROF_IO] = "io",
+  [PROF_IO_READ] = "io/read",
+  [PROF_IO_UNPACK] = "io/unpack",
+  [PROF_IO_PACK] = "io/pack",
+  [PROF_IO_PROCESS] = "io/process",
+  [PROF_IO_DEFERRED] = "io/deferred",
+  [PROF_IO_UPD_FDSET] = "io/upd_fdset",
+  [PROF_IO_CLIENT] = "io/client",
+  [PROF_IO_MEMMOVE] = "io/memmove",
+  [PROF_IO_SEND] = "io/send",
+  [PROF_DRAW] = "draw",
+  [PROF_DRAW_BLIT] = "draw/blit",
+  [PROF_UPDATE] = "update",
+  [PROF_UPDATE_POS] = "update/pos",
 };
 
 void profile_start(int track);
 void profile_end(int track);
 void profile_show(void);
+void profile_end2(int track);
